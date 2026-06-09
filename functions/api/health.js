1641f96f266a196d305bf1583f32427108a1b395
FaytSystems <robertreese@faytsystems.com>
2026-06-09 07:49:29 +0000

    Upload ChefPuppers smart checkout site

--- functions/api/health.js
+++
@@ -1,8 +0,0 @@
-export function onRequestGet() {
-  return new Response(JSON.stringify({ ok: true, service: "chefpuppers-dynamic-checkout" }), {
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      "Cache-Control": "no-store"
-    }
-  });
-}
